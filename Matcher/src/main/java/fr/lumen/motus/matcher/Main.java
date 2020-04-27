3ada0d91e0a14a81aab5dbedd593c1f6e221304d
Louis-Maxime Crédeville <credeville.louis.maxime@gmail.com>
2020-04-27 21:16:51 +0200

    Solver

---
+++ Matcher/src/main/java/fr/lumen/motus/matcher/Main.java
@@ -0,0 +1,7 @@
+package fr.lumen.motus.matcher;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Matcher");
+    }
+}
---
+++ Matcher/src/main/java/fr/lumen/motus/matcher/Matcher.java
@@ -0,0 +1,37 @@
+package fr.lumen.motus.matcher;
+
+public class Matcher {
+    private final String word;
+
+    public Matcher(String word) {this.word = word;}
+
+    public String result(String proposition) {
+        assert word.length() == proposition.length();
+        StringBuilder result = new StringBuilder(word.length());
+        for (int i = 0; i < word.length(); i++) {
+            final char c = proposition.charAt(i);
+            if (word.charAt(i) == c) {
+                result.append('R');
+            } else if (containsBadPlaced(c, proposition)) {
+                result.append('J');
+            } else {
+                result.append('B');
+            }
+        }
+        return result.toString();
+    }
+
+    private boolean containsBadPlaced(char c, String proposition) {
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == c && proposition.charAt(i) != c) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+}
